55386af9011fc981136b164f55c93e30b3173aca
statisfactions <ethancbrown@gmail.com>
2011-07-12 00:23:53 +0000

    Made some changes to aaa.R before in last commit to try and look for 'aplay' and use it as the default player, but that made R CMD check go bonkers so I ditched it (and related functionality in the documentation.

--- R/aaa.R
+++ R/aaa.R
@@ -6,8 +6,5 @@
       ## We only set the option if the system is NOT
       ## Linux. audio::play does not work well with Linux.
       options(player = "audio::play")
-    } else if(system2("which", "aplay") == 0){
-      ## Check to see if aplay is installed if so use it.
-      options(player = "aplay")
-    }
+    } 
 }
